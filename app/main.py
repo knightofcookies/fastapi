60b45524e815b97c3f1335d3e2c455c3eae52eb7
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2023-10-21 16:09:14 +0530

    Add CORSMiddleware support

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import post, user, auth, vote
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Blog")
+
+origins = ["*"]
+
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"], allow_headers=["*"],)
 
 app.include_router(post.router)
 app.include_router(user.router)
