689865289f460a7166b01c65e0fb01e30bf8f746
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2023-10-23 15:14:27 +0530

    Fix duplicate email entry error and registration bug

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,22 +1,34 @@
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter, status, HTTPException
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oauth2
 from .. import database
 
-router = APIRouter(prefix="/users", tags=['Users'])
+router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Token)
 def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
     user_dict = user.model_dump()
+
+    user_query = db.query(models.User).filter(models.User.email == user.email).first()
+
+    if user_query is not None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"User with email {user.email} already exists",
+        )
+
     new_user = models.User(**user_dict)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return new_user
+
+    access_token = oauth2.create_access_token(data={"user_id": new_user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @router.get("/{id}", response_model=schemas.UserOut)
